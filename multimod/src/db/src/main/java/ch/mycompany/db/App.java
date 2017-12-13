6c38179209dc7683ec0796301ced0af9710dc8f4
Herr Kleisli <herrkleisli@gmail.com>
2017-12-13 15:50:41 +0100

    some bugs

--- multimod/src/db/src/main/java/ch/mycompany/db/App.java
+++ multimod/src/db/src/main/java/ch/mycompany/db/App.java
@@ -4,9 +4,22 @@ package ch.mycompany.db;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        int i = 42;
+        try {
+        } catch (Exception e) {
+        }
+    }
+
+    public void foo () {
+    }
+
+    public void foo2 () {
+    }
+    private void foo3() {
+        // no idea why this is empty
     }
 }
--- multimod/src/gui/src/main/java/ch/mycompany/gui/App.java
+++ multimod/src/gui/src/main/java/ch/mycompany/gui/App.java
@@ -6,9 +6,35 @@ package ch.mycompany.gui;
  */
 public class App 
 {
+    double d = Double.longBitsToDouble(42);  // Noncompliant
     public static void main( String[] args )
     {
+        int i = 42;
+        double d = Double.longBitsToDouble(i);  // Noncompliant
+        System.out.println( "Hello World!" );
+    }
 
+    private void untested() {
+        try {
+            System.out.println( "Hello World!" );
+        } catch(Exception e) {
+            int i = 42;
+            double d = Double.longBitsToDouble(i);  // Noncompliant
+        }
+    }
+
+    private void foo() {
+        int i = 100023;
+    }
+
+    private void bar() {
+    }
+
+    private void foobar() {
+        int gugus = 6666;
+        //FIXME fix this
+        float pi = 3.1415f;
+        double e = 2.718;
     }
 
 }
